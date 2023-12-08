61f65fbf3c0c559db79a3d527f6cc94dff112ca1
Bartosz Nowak <bartoszn337@gmail.com>
2023-12-08 17:37:55 +0100

    not working yet

---
+++ COMPILE.sh
@@ -0,0 +1,19 @@
+cairo-compile --cairo_path=./src src/starkware/cairo/cairo_verifier/layouts/all_cairo/cairo_verifier.cairo --output cairo_verifier.json --no_debug_info
+
+cairo-run \
+    --program=cairo_verifier.json \
+    --layout=small \
+    --program_input=cairo_verifier_input.json \
+    --air_public_input=cairo_verifier_public_input.json \
+    --air_private_input=cairo_verifier_private_input.json \
+    --trace_file=cairo_verifier_trace.json \
+    --memory_file=cairo_verifier_memory.json \
+    --print_output
+
+cairo-run \
+    --program=cairo_verifier.json \
+    --layout=starknet_with_keccak \
+    --program_input=cairo_verifier_input.json \
+    --trace_file=cairo_verifier_trace.json \
+    --memory_file=cairo_verifier_memory.json \
+    --print_output
