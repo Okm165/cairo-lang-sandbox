12e482de1ba29f96f5bc9f709a0c26fd164cf537
Bartosz Nowak <bartekn337@gmail.com>
2023-12-14 19:22:26 +0000

    fixes

--- COMPILE.sh
+++ COMPILE.sh
@@ -1,16 +1,5 @@
 cairo-compile --cairo_path=./src src/starkware/cairo/cairo_verifier/layouts/starknet_with_keccak/cairo_verifier.cairo --output cairo_verifier.json --no_debug_info
 
-# General
-cairo-run \
-    --program=cairo_verifier.json \
-    --layout=small \
-    --program_input=cairo_verifier_input.json \
-    --air_public_input=cairo_verifier_public_input.json \
-    --air_private_input=cairo_verifier_private_input.json \
-    --trace_file=cairo_verifier_trace.json \
-    --memory_file=cairo_verifier_memory.json \
-    --print_output
-
 cairo-run \
     --program=cairo_verifier.json \
     --layout=starknet_with_keccak \
